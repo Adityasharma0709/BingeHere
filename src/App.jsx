f7e71e1125b58b16cb90dd93f95a9d6fda776ce1
SAHIL12343 <agarwalsahil186@gmail.com>
2026-02-12 14:28:59 +0530

    complete code

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
+import SeatSelection from "./pages/SeatSelection";
+import Contact from "./pages/Contact";
+import About from "./pages/About"; 
+
 function App() {
-  return <>
-    {/* <SeatSelection/> */}
-    {/* <h1 class="text-3xl font-bold underline">    Hello world!  </h1> */}
-    <LandingPage/>
-  </>;
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/about" element={<About/>} />
+    </Routes>
+  );
 }
 
 export default App;
+``
